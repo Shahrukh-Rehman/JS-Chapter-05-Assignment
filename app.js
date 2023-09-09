82d32dbf849bddbd935830b34089496a8c955056
Shahrukh-Rehman <shahrukhrehman321@gmail.com>
2023-09-10 01:17:50 +0500

    Question 1 to 13

---
+++ app.js
@@ -0,0 +1,158 @@
+// Q1
+// var addition = 3 + 5
+// document.write("Sum of 3 and 5 is " + addition)
+
+
+// Q2
+// var subtraction = 3 - 5
+// document.write("Subtraction of 3 and 5 is " + subtraction);
+
+// var multiplication = 3 * 5
+// document.write("Multiplication of 3 and 5 is " + multiplication);
+
+// var division = 3 / 5
+// document.write("Division of 3 and 5 is " + division);
+
+
+// Q3
+// var value;
+// document.write("Value after variable declaration is " + value)
+
+// document.write("<br>");
+
+// value = 5;
+// document.write("Intial Value: " + value)
+
+// document.write("<br>");
+
+// var value2 = ++value;
+// document.write("Value after increment is: " + value2)
+
+// document.write("<br>");
+
+// var value3 = value + 7;
+// document.write("Value after addition is: " + value3);
+
+// document.write("<br>");
+
+// var value4 = --value3;
+// document.write("Value after decrement is: " + value4);
+
+// document.write("<br>");
+
+// var value5 = value4 % 3;
+// document.write("The remainder is: " + value5);
+
+
+// Q4
+
+// var ticket = 600
+// var buyingTickets = 5
+// var result = 600 * 5
+// document.write("Total cost to buy 5 tickets to a movie is " + result + "PKR")
+
+
+// Q5
+// var table = 4;
+// var counting = [1,2,3,4,5,6,7,8,9,10];
+
+// document.write("Table of " + table);
+// document.write("<br>");
+// document.write(table + "x" + counting[0] + "=" + table * counting[0]);
+// document.write("<br>");
+// document.write(table + "x" + counting[1] + "=" + table * counting[1]);
+// document.write ("<br>");
+// document.write(table + "x" + counting[2] + "=" + table * counting[2]);
+// document.write("<br>");
+// document.write(table + "x" + counting[3] + "=" + table * counting[3]);
+// document.write("<br>");
+// document.write(table + "x" + counting[4] + "=" + table * counting[4]);
+// document.write("<br>");
+// document.write(table + "x" + counting[5] + "=" + table * counting[5]);
+// document.write("<br>");
+// document.write(table + "x" + counting[6] + "=" + table * counting[6]);
+// document.write("<br>");
+// document.write(table + "x" + counting[7] + "=" + table * counting[7]);
+// document.write("<br>");
+// document.write(table + "x" + counting[8] + "=" + table * counting[8]);
+// document.write("<br>");
+// document.write(table + "x" + counting[9] + "=" + table * counting[9]);
+
+
+// Q6
+// var celsius = 25
+// var fahrenheit =  70
+// var C = (fahrenheit - 32) * 5 / 9;
+// var F = (celsius * 9 / 5) + 32
+// document.write(celsius + "\u00B0C " + "is " + F + "\u00B0F")
+// document.write("<br>")
+// document.write(fahrenheit + "\u00B0F " + "is " + C + "\u00B0C")
+
+
+// Q7
+// var item1 = 650
+// var item2 = 100
+// var orderedItem1 = 3
+// var orderedItem2 = 7
+// var shippingCharges = 100
+// var cost = (item1 * orderedItem1) + (item2 * orderedItem2) + shippingCharges
+// document.write("Price of item 1 is " + item1 + "<br/>")
+// document.write("Quantity of item 1 is " + orderedItem1 + "<br/>")
+// document.write("Price of item 2 is " + item2 + "<br/>")
+// document.write("Quantity of item 2 is " + orderedItem2 + "<br/>")
+// document.write("Shipping Charges is " + shippingCharges + "<br/>" + "<br/>")
+// document.write("Total cost of your order is " + cost)
+
+
+// Q8
+// var totalMarks = 980
+// var obtainedMarks = 804
+// var percentage = obtainedMarks / totalMarks * 100
+// document.write("Total marks: " + totalMarks + "<br/>")
+// document.write("Marks obtained: " + obtainedMarks + "<br/>")
+// document.write("Percentage: " + percentage + "%")
+
+
+// Q9
+// var oneUS_Dollar = 307.47
+// var oneSaudi_Riyal = 81.97
+// var exchangedCurrency = (oneUS_Dollar * 10) + (oneSaudi_Riyal * 25)
+// document.write("Total Currency in PKR: " + exchangedCurrency)
+
+
+// Q10
+// var num1 = 10
+// var calculation = (((num1 + 5) * 10) / 2)
+// document.write("Result " + calculation)
+
+
+// Q11
+// var currentYear = 2023
+// var birthYear = 1998
+// var yourAge = currentYear - birthYear
+// document.write("Current Year: " + currentYear + "<br/>")
+// document.write("Birth Year: " + birthYear + "<br/>")
+// document.write("Your age is: " + yourAge)
+
+// Q12
+// var radius = 20
+// var pi = 3.142
+// var circumference = 2 * pi * radius
+// var area = pi * radius * radius
+// document.write("Radius of a circle: " + radius + "<br/>")
+// document.write("The circumference is: " + circumference + "<br/>")
+// document.write("The area is: " + area)
+
+
+// Q13
+// var snack = "chocolate chip"
+// var currentAge = 25
+// var maximumAge = 65
+// var snacksPerDay = 3
+// var totalDaysInYear = 365
+// var totalSnacks = (maximumAge-currentAge) * totalDaysInYear * snacksPerDay
+// document.write("Favourite Snack: " + snack + "<br/>")
+// document.write("Current age: " + currentAge + "<br/>")
+// document.write("Estimated Maximum Age: " + maximumAge + "<br/>")
+// document.write("Amount of snacks per day: " + snacksPerDay + "<br/>")
+// document.write("You will need " + totalSnacks + " chocolate chip to last you until the ripe old age of " + maximumAge)
